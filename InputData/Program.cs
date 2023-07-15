e394fb6083d9eda2d36d1bb265f50fa63865662d
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-15 16:16:30 +0300

    Proje dosyası ekle.

---
+++ InputData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sehir,ilce;
+            Console.WriteLine("Lütfen sehrinizi giriniz : ");
+            sehir = Console.ReadLine();
+            Console.WriteLine("Lütfen ilcenizi giriniz : ");
+            ilce = Console.ReadLine();
+            Console.WriteLine("Girmis oldugunuz sehir ve ilce : " + sehir + "/" + ilce);
+            
+
+            Console.Read();
+        }
+    }
+}
